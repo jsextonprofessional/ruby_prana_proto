d9d540fa3ace5d5d57f09d9d7efb4c8ca8746598
Jacob Sexton <jshuman@protonmail.com>
2021-09-02 18:56:46 +0000

    Asanas controller filled out - basic CRUD functionality is working. Added navbar partial. Created show_all method and view to display Asana.all

--- prana/app/controllers/asanas_controller.rb
+++ prana/app/controllers/asanas_controller.rb
@@ -1,2 +1,51 @@
 class AsanasController < ApplicationController
+    layout "asanas"
+    skip_before_action :verify_authenticity_token
+    
+    def index
+    
+    end
+
+    def new
+    
+    end
+    
+    def create
+        @asana = Asana.new(asana_params)
+        if @asana.save
+            redirect_to "/asanas/#{@asana.id}"
+        else
+            flash[:errors] = @asana.errors.full_messages
+            redirect_to "/asanas/new"
+        end
+    end
+
+    def show
+        @asana = Asana.find(params[:id])
+    end
+
+    def show_all
+        @asanas = Asana.all
+    end
+
+    def edit
+        @asana = Asana.find(params[:id])
+    end
+
+    def update
+        @asana = Asana.find(params[:id])
+        @asana.update(asana_params)
+        redirect_to "/asanas/#{@asana.id}"
+    end
+
+    def destroy
+        @asana = Asana.find(params[:id])
+        @asana.destroy
+        redirect_to "/asanas"
+    end
+
+    private
+        def asana_params
+            params.require(:asana).permit(:asana_name, :asana_type, :target_general, :target_specific, :difficulty, :duration)
+        end
 end
--- prana/app/models/asana.rb
+++ prana/app/models/asana.rb
@@ -1,2 +1,9 @@
 class Asana < ActiveRecord::Base
+
+    validates :asana_name, presence: true, uniqueness: true
+    validates :asana_type, presence: true
+    validates :target_general, presence: true
+    validates :target_specific, presence: true
+    validates :difficulty, presence: true
+    validates :duration, presence: true
 end
