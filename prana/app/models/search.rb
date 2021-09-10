ff790336b557464609485d0af0a2e8198f29f8f6
Jacob Sexton <jshuman@protonmail.com>
2021-09-10 19:15:36 +0000

    3/4 advanced search routes working

--- prana/app/models/search.rb
+++ prana/app/models/search.rb
@@ -4,11 +4,12 @@ class Search < ActiveRecord::Base
         asanas = Asana.all
 
         asanas = asanas.where(["asana_name LIKE ?","%#{keywords}%"]) if keywords.present?
-        asanas = asanas.where(["asana_type LIKE ?", asana_type]) if asana_type.present?
-        asanas = asanas.where(["target_general LIKE ?", target_general]) if target_general.present?
-        asanas = asanas.where(["target_specific LIKE ?", target_specific]) if target_specific.present?
-        asanas = asanas.where(["difficulty LIKE ?", difficulty]) if difficulty.present?
-        asanas = asanas.where(["duration = ?", duration]) if duration.present?
+        asanas = asanas.where(["asana_name LIKE ?", "%#{asana_name}%"]) if asana_name.present?
+        asanas = asanas.where(["asana_type LIKE ?", "%#{asana_type}%"]) if asana_type.present?
+        asanas = asanas.where(["target_general LIKE ?", "%#{target_general}%"]) if target_general.present?
+        asanas = asanas.where(["target_specific LIKE ?", "%#{target_specific}%"]) if target_specific.present?
+        asanas = asanas.where(["difficulty LIKE ?", "%#{difficulty}%"]) if difficulty.present?
+        asanas = asanas.where(["duration = ?", "%#{duration}%"]) if duration.present?
 
 
         return asanas
